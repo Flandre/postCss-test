d822cfddfcf87b1ec7685a0b0669c152ade3c4a2
Flandre <flantan233@gmail.com>
2016-07-19 17:12:10 +0800

    add grunt

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,18 @@
-var gulp = require('gulp')
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnext = require('cssnext');
+var precss = require('precss');
+gulp.task('css', function(){
+  var processors = [
+    autoprefixer,
+    cssnext,
+    precss
+  ];
+  return gulp.src('./src/*.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./dest'));
+});
 gulp.task('default', function(){
-
+  gulp.start('css');
 });
